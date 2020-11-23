8942b5367f41e4114b304fdef59fadc00739dbf9
DmitroYakimov <56223340+DmitroYakimov@users.noreply.github.com>
2020-11-23 15:53:47 +0200

    Add files via upload

---
+++ Lab2.1.5(4).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello, enter year: ";
+	int year;
+	cin >> year;
+	cout << "Enter month: ";
+	int month;
+	cin >> month;
+	cout << "Enter day: ";
+	int day;
+	cin >> day;
+	month = month -2;
+	if(month < 0){
+	    month = month * 12;
+	    year = year - 1;
+	}
+	month = month* 83/32;
+	month = month + day;
+	month = month + year;
+	month = month + (year/4);
+	month = month + (year/100);
+	month = month + (year/400);
+	int ost;
+	ost = month % 7;
+	switch(ost) {
+	 case 0: cout << "Your day is Sunday";
+	 break;
+	 case 1: cout << "Your day is Monday";
+	 break;
+	 case 2: cout << "Your day is Tuesday";
+	 break;
+	 case 3: cout << "Your day is Wednesday";
+	 break;
+	 case 4: cout << "Your day is Thursday";
+	 break;
+	 case 5: cout << "Your day is Friday";
+	 break;
+	 case 6: cout << "Your day is Saturday";
+	 break;
+	}
+}
---
+++ Lab2.9.8(3).cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void){
+    int vector[] = {1,7,3,8,3,7,1};
+    bool palindrom = false;
+    int n = 
+    for(int i = 0; i < sizeof(vector); i++){
+        for(int j = sizeof(vector); j>0; j--){
+            if(vector[i] == vector[j]){
+                palindrom = true;
+            }
+            else{palindrom = false;}
+            break;
+        }
+    }
+    if(palindrom == true) {cout<< "It's palindrom";}
+    else{cout << "Not palinrom";}
+    return 0;
+}
